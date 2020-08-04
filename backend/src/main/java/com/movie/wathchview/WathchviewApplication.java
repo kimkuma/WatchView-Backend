4747c1b2f1e66388a6ba1ceacce0fea0e1dbfc45
HyunJoon,Kim <kimkuma04@gmail.com>
2020-08-05 00:16:08 +0900

    first commit

---
+++ backend/src/main/java/com/movie/wathchview/WathchviewApplication.java
@@ -0,0 +1,13 @@
+package com.movie.wathchview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WathchviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WathchviewApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/movie/wathchview/WathchviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movie.wathchview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WathchviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
